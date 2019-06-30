10363c95fdefee7e63b61470216f2391e5d3846a
Gabriel Mijares <gabrielmljob@gmail.com>
2019-06-30 17:08:33 +0200

    create functional component in Greeting folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Greeting from '../src/Greeting/Greeting';
 
 class App extends Component {
-  render(){
-    return (
-      <p>Hello World!</p>
-    );
-  }
+    render() {
+        return (
+          //<p> Hello World! </p>
+          <Greeting/>
+        );
+    }
 }
 
 export default App;
---
+++ src/Greeting/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const name = 'Gabriel';
+
+let Greeting = () => {
+	return <p>Hello {name}!</p>
+}
+
+export default Greeting;
