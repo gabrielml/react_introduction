57b7dfb07bb591587cef9143529c8a34414c20ed
Gabriel Mijares <gabrielmljob@gmail.com>
2019-07-15 20:01:35 +0200

    react with redux

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Home';
-import Products from './Products';
+import Products from '../Containers/Products';
 import Orders from './Orders';
 
 const Main = () => {
---
+++ src/Components/Product.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Product = (props) => (
+	<div>
+		<p>{props.name}: {props.cost} $</p>
+	</div>
+)
+
+export default Product;
